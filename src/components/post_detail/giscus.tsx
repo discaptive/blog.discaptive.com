9007dcf4f93208b4664cf9dc19cbca518fb1032e
discaptive <discaptive@gmail.com>
2024-10-18 17:53:27 +0900

    Update giscus.tsx

--- src/components/post_detail/giscus.tsx
+++ src/components/post_detail/giscus.tsx
@@ -31,7 +31,7 @@ export default function Giscus() {
     scriptElem.setAttribute("data-strict", "0");
     scriptElem.setAttribute("data-reactions-enabled", "1");
     scriptElem.setAttribute("data-emit-metadata", "0");
-    scriptElem.setAttribute("data-input-position", "bottom");
+    scriptElem.setAttribute("data-input-position", "top");
     scriptElem.setAttribute("data-theme", theme);
     scriptElem.setAttribute("data-lang", "ko");
 
