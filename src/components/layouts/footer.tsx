38a844857a24e31a68aed15f33ae0787784ccded
discaptive <discaptive@gmail.com>
2024-10-28 01:27:38 +0900

    Create footer.tsx

---
+++ src/components/layouts/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="my-16 max-w-6xl px-8 mx-auto w-full flex flex-col items-center justify-center text-center print:hidden">
+      <div>
+        © 2024. <span className="font-semibold">Discaptive</span> All rights
+        reserved.
+      </div>
+    </footer>
+  );
+}
