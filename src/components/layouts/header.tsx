9db945d8c6a3363e5f4eae8db5a0867d979aa32e
discaptive <discaptive@gmail.com>
2024-10-28 03:28:09 +0900

    Update header.tsx

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -14,21 +14,19 @@ export default function Header() {
 
   return (
     <header className="fixed z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 print:hidden">
-      <div className="container items-center flex h-16 max-w-6xl justify-between px-8">
-        <Button variant="link" className="px-2">
-          <Link href="/" className="flex items-center gap-2">
-            <Image
-              className={`w-9 h-9 ${isDarkMode ? "filter invert" : ""}`}
-              src={"/logo.png"}
-              alt={"Logo"}
-              width={256}
-              height={256}
-            />
-            <span className="hidden text-base font-medium sm:inline-block">
-              Discaptive
-            </span>
-          </Link>
-        </Button>
+      <div className="container items-center flex h-16 max-w-6xl justify-between px-6">
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            className={`w-9 h-9 ${isDarkMode ? "filter invert" : ""}`}
+            src={"/logo.png"}
+            alt={"Logo"}
+            width={256}
+            height={256}
+          />
+          <span className="hidden text-base font-medium sm:inline-block">
+            Discaptive
+          </span>
+        </Link>
 
         <div className="flex items-center">
           <Button variant="link">
